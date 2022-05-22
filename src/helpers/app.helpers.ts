b70fe3a2652e5bb388d03cdb3003156869fd6c07
Laurent LELION <laurent.lelion@momji.fr>
2022-05-22 11:48:56 +0200

    fixed generateShortURL with /

--- src/helpers/app.helpers.ts
+++ src/helpers/app.helpers.ts
@@ -5,10 +5,18 @@ import crypto from 'crypto';
  * @param {String} longURL
  * @param {String} startIndex
  * @param {Number} endIndex
+ * @param {String} salt
  * @returns {String}
  */
-const generateShortURL = (longURL: string, startIndex = 0, endIndex = 10): string => {
-    const hash = crypto.createHash('md5').update(longURL).digest('base64');
+const generateShortURL = (longURL: string, startIndex = 0, endIndex = 10, salt = ''): string => {
+    let hash = crypto.createHash('md5').update(`${salt}${longURL}`).digest('base64');
+
+    // remove all "/"
+    const searchRegExp = /\//g;
+    const replaceWith = '';
+
+    hash = hash.replace(searchRegExp, replaceWith);
+
     return hash.substring(startIndex, endIndex + 1);
 };
 
